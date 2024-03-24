34e961e9b5583af1a87859baa5baa75f90651d2e
Hieutapcode3 <hieupham9155@gmail.com>
2024-03-24 21:49:23 +0700

    change class name

--- src/ClientSv/ServerManager.java
+++ src/ClientSv/ServerManager.java
@@ -16,11 +16,11 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author phamt
  */
-public class ManagerServer extends javax.swing.JFrame {
+public class ServerManager extends javax.swing.JFrame {
     private ArrayList<Server> list ;
     private boolean[] statecomputer = new boolean[20];
     DefaultTableModel model;
-    public ManagerServer() {
+    public ServerManager() {
         initComponents();
         this.setLocationRelativeTo(null);
         list  = new ArrayList<>();
@@ -57,7 +57,6 @@ public class ManagerServer extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1200, 700));
 
         BtnSort.setText("Sắp Xếp");
         BtnSort.addActionListener(new java.awt.event.ActionListener() {
@@ -494,21 +493,23 @@ public class ManagerServer extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ManagerServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ServerManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ManagerServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ServerManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ManagerServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ServerManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ManagerServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ServerManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ManagerServer().setVisible(true);
+                new ServerManager().setVisible(true);
             }
         });
     }
